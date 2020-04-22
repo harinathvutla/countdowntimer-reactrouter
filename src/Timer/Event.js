7e502bc2acb419d171ef0523c6e2bf0c1552eed2
Harinath Vutla <h.vutla@douglas.de>
2020-04-22 10:54:48 +0200

    implemented most of the user stories apart from bonus

--- src/Timer/Event.js
+++ src/Timer/Event.js
@@ -3,76 +3,103 @@ import React, { Component } from 'react';
 class Event extends Component {
     state={
         datetime: new Date(),
-        monthDays:[{'1': 31},{'2': 28},{'3': 31},{'4': 30},{'5': 31},{'6': 30},{'7': 31},{'8': 31},{'9': 30},{'10': 31},{'11': 30},{'12': 31}]
+        monthDays:[{'1': 31},{'2': 28},{'3': 31},{'4': 30},{'5': 31},{'6': 30},{'7': 31},{'8': 31},{'9': 30},{'10': 31},{'11': 30},{'12': 31}],
+        eventname: '',
+        dateCDNSet: '',
+        timeCDNSet: '' 
     }
 
+    // Today Date and Time display logic
     componentDidMount=()=>{
         this.timerID= setInterval(()=>this.setTime(),1000);
     }
+
+    setTime=()=> {
+        this.setState({
+         datetime: new Date() ,
+          h : this.state.datetime.getHours(),
+         m : this.state.datetime.getMinutes(),
+         s : this.state.datetime.getSeconds()   
+       });
+     }
 
     componentWillUnmount() {
         clearInterval(this.timerID);
       }
 
-    setTime=()=> {
-         this.setState({
-          datetime: new Date() ,
-           h : this.state.datetime.getHours(),
-          m : this.state.datetime.getMinutes(),
-          s : this.state.datetime.getSeconds()   
-        });
-      }
 
-       countDown=()=>{
-         let totalCDNDays=0,firstdayCDNHrs=0,lastdayCDNHrs=0;
-        for(let i= this.state.datetime.getMonth()+1; i<= parseInt(this.state.dateCDNSet.split('-')[1]);i++ )
-        {
-            if(i===this.state.datetime.getMonth()+1)
-            {
-                totalCDNDays+=Object.values(this.state.monthDays[i-1])[0] - this.state.datetime.getDate();
-            }
-            else if(i===parseInt(this.state.dateCDNSet.split('-')[1]))
-            {
-                totalCDNDays+=parseInt(this.state.dateCDNSet.split('-')[2])
-            }
-            else
-            {
-                totalCDNDays+=Object.values(this.state.monthDays[i-1])[0];
-            }
-        } 
+    // count down timer logic
+       countDown=(countDownDate)=>{
        
-        
- /*        this.setState({
-            days: this.state.dateCDN.split
-        }) */
+        let timediff= countDownDate-this.state.datetime.getTime();
+        console.log(countDownDate,this.state.datetime.getTime(),timediff);
+        // Count down time calculations for days, hours, minutes and seconds
+        if(timediff>=0)
+        {
+            let cdnDays = Math.floor(timediff / (1000 * 60 * 60 * 24));
+            let cdnHours = Math.floor((timediff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+            let cdnMinutes = Math.floor((timediff % (1000 * 60 * 60)) / (1000 * 60));
+            let cdnSeconds = Math.floor((timediff % (1000 * 60)) / 1000);
 
-       // console.log(totalCDNDays);
+            console.log(cdnDays,cdnHours,cdnMinutes,cdnSeconds);
+            this.setState({
+            cdnDays:cdnDays,  
+            cdnHours: cdnHours,
+            cdnMinutes: cdnMinutes,
+            cdnSeconds: cdnSeconds,
+            timediff: timediff
+            });
+        }
+        else{
+            clearInterval(this.cdnTimer);
+        }
+    } 
 
-       let cdnHours=parseInt(this.state.timeCDNSet.split(':')[0])-this.state.h;
-       let cdnMinutes=parseInt(this.state.timeCDNSet.split(':')[1])-this.state.m-1;   
-       let cdnSeconds = 59-this.state.s;
-      
-       console.log(cdnMinutes,parseInt(this.state.timeCDNSet.split(':')[1]),this.state.m,this.state.timeCDNSet);
-
-       this.setState({
-        cdnHours: cdnHours,
-        cdnMinutes: cdnMinutes,
-        cdnSeconds: cdnSeconds
-    });
-
-      } 
-
- 
-   componentDidUpdate=()=>{
-       if(this.state.cdnHours===0 && this.state.cdnMinutes===0 && this.state.cdnSeconds===0  )
-       clearInterval(this.cdnTimer);
-   }
 
     formSubmitHandler=e=>{
         e.preventDefault();
-      //  console.log(e);
-      this.cdnTimer=setInterval(()=>this.countDown(),1000);
-      //this.countDown();
+ 
+        let year= parseInt(this.state.dateCDNSet.split('-')[0]);
+        let month= parseInt(this.state.dateCDNSet.split('-')[1]);
+        let day= parseInt(this.state.dateCDNSet.split('-')[2]);
+        let hours= parseInt((this.state.timeCDNSet!=='')?this.state.timeCDNSet.split(':')[0]:'0');
+        let mins=parseInt((this.state.timeCDNSet!=='')?this.state.timeCDNSet.split(':')[1]:'0');
+
+        console.log(year,month,day,hours,mins);
+        let countDownDate=new Date(year,month-1,day,hours,mins,0).getTime();
+        if((countDownDate-this.state.datetime.getTime())>=0)
+        {
+            if(this.state.eventname!=='')
+            {
+                this.cdnTimer=setInterval(()=>this.countDown(countDownDate),1000);
+                this.setState({
+                    warnMessage: ''
+                  });
+            }
+            else
+            {
+                this.setState({
+                    warnMessage: 'Event name cannot be blank!'
+                  });               
+            }
+
+        }
+        else
+        {
+            if(this.state.eventname!=='')
+            {
+                this.setState({
+                    warnMessage: 'Please enter the date and time correctly! '
+                });
+            }
+            else
+            {
+                this.setState({
+                    warnMessage: 'Event name cannot be blank and Please enter the date and time correctly! '
+                });
+            }
+
+        }
      
     }
 
@@ -84,7 +111,7 @@ class Event extends Component {
     }
 
     render() {
-      //  console.log(this.state);
+        console.log(this.state);
        // console.log(this.state.datetime.toLocaleTimeString());
         return (
             <div>
@@ -97,25 +124,29 @@ class Event extends Component {
                     <label>Date: <input className="inputs" type="date" name="dateCDNSet" value ={this.state?.dateCDNSet??''} onChange={this.handleChange}></input> </label>
                     <label>Time: <input className="inputs" type="time" name="timeCDNSet" value ={this.state?.timeCDNSet??''} onChange={this.handleChange}></input> </label>
                     </div>
+                    <h4 style={{color:'red'}}>{this.state?.warnMessage??''}</h4>
                     <input type="submit" value="Start" className="button"/>
                 </form>
+                <div className='cntdwn-center'>
                 <div className="cntdwn">
+        <h4>{this.state?.eventname??''} {this.state?.dateCDNSet??''} {this.state?.timeCDNSet??''}</h4>
                     <div>
-                        <h5>Days</h5>
-                        <h2>TBD</h2>
+                        <h3>Days</h3>
+                        <p>{this.state?.cdnDays??'00'}</p>
                     </div>
                     <div>
-                        <h5>Hours</h5>
-                        <h2>{this.state?.cdnHours??'00'}</h2>
+                        <h3>Hours</h3>
+                        <p>{this.state?.cdnHours??'00'}</p>
                     </div>
                     <div>
-                        <h5>Minutes</h5>
-                        <h2>{this.state?.cdnMinutes??'00'}</h2>
+                        <h3>Minutes</h3>
+                        <p>{this.state?.cdnMinutes??'00'}</p>
                     </div> 
                     <div>
-                        <h5>Seconds</h5>
-                        <h2>{this.state?.cdnSeconds??'00'}</h2>
+                        <h3>Seconds</h3>
+                        <p>{this.state?.cdnSeconds??'00'}</p>
                     </div>                    
+                </div>
                 </div>
             </div>
         );
